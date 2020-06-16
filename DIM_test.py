706d67f43e87ab9bd247b3295e95031a1d9ef2bd
wicebing <32655081+wicebing@users.noreply.github.com>
2020-06-16 22:20:04 +0800

    Add files via upload

--- DIM_test.py
+++ DIM_test.py
@@ -20,6 +20,12 @@ from torch.utils.data import Dataset, DataLoader
 import matplotlib.pyplot as plt
 from PIL import Image
 
+try:
+    task = sys.argv[1]
+    print('*****task= ',task)
+except:
+    task = 'test'
+
 CIFAR10 = torchvision.datasets.CIFAR10(
     './CIFAR10', train=True, download=True)
 
@@ -45,9 +51,9 @@ class MyDataset(Dataset):
     def __len__(self):
         return len(self.imgs)
 
-batch_size = 4
+batch_size = 8
 lr = 1e-5
-checkpoint_file = './checkpoint' 
+
 parallel= False
 device = 'cuda'
 
@@ -59,6 +65,9 @@ data_loader_train = DataLoader(train_data, batch_size=batch_size,shuffle=True)
 test_data=MyDataset(ds=CIFAR10_test, transform=transform)
 data_loader_test = DataLoader(test_data, batch_size=batch_size,shuffle=False)
 
+
+def count_parameters(model):
+    return sum(p.numel() for p in model.parameters() if p.requires_grad)  
 
 def save_checkpoint(checkpoint_file,checkpoint_path, model, parallel, optimizer=None):
     if parallel:
@@ -88,7 +97,8 @@ def load_checkpoint(checkpoint_file,checkpoint_path, model):
     print('model saved to %s / %s' % (checkpoint_file,name_))
     return model
 
-def plot_2d(feature,sex_tlabel, ep): 
+def plot_2d(feature,sex_tlabel, ep,picpath='./pic/'): 
+    print('** draw 2d **')
     import matplotlib.pyplot as plt
     
     dim_len = feature.shape[1]
@@ -97,26 +107,26 @@ def plot_2d(feature,sex_tlabel, ep):
     fig = plt.figure(figsize=(48,48),dpi=100)
     
     for p in range(pic_len):
-        pn = '77{:.0f}'.format(p+1)
-        xi = p%7
-        yi = int(p/7)
-        ax = plt.subplot2grid((7,7),(yi,xi))
+        pn = '66{:.0f}'.format(p+1)
+        xi = p%6
+        yi = int(p/6)
+        ax = plt.subplot2grid((6,6),(yi,xi))
         #ax = fig.add_subplot(int(pn))
         for tril in range(10):
             cls = sex_tlabel==tril        
             ax.scatter(feature[cls,2*p],feature[cls,2*p+1],
-                       marker = '.', alpha=0.3)    
+                       marker = '.', alpha=0.3,label=str(tril))    
         plt.title('EDisease (cifar10) '+str(p))     
-                    
-    plt.savefig('./pic/'+'EDisease_2d_cifar10_'+str(ep)+'.png')
-    plt.show()
+        plt.legend()
+    plt.savefig(picpath+'EDisease_2d_cifar10_'+str(ep)+'.png')
+    # plt.show()
     plt.close()
 
 
 class encoder(nn.Module):
     def __init__(self):
         super(encoder, self).__init__()
-        self.encode0 = nn.Sequential(nn.Conv2d(3, 1, 1),
+        self.encode0 = nn.Sequential(nn.Conv2d(1, 1, 1),
                                      nn.GELU(),
                                      nn.Dropout(0.5),
                                      nn.Conv2d(1, 1, 1),
@@ -148,34 +158,92 @@ class encoder(nn.Module):
         bs = len(img)
         img = self.encode0(img)
         output = self.encode1(img)
-        return output.view(bs,-1), img.view(bs,-1)
+        return output.view(bs,-1), img#.view(bs,-1)
+
+class decoder(nn.Module):
+    def __init__(self,device='cpu'):
+        super(decoder, self).__init__()
+        self.decoder = nn.Sequential(nn.ConvTranspose2d(64,64,kernel_size=3, stride=1, padding=0),
+                                     nn.GELU(),
+                                     nn.Dropout(0.5),
+                                     nn.BatchNorm2d(64),
+                                     nn.ConvTranspose2d(64,64,kernel_size=3, stride=2, padding=0),
+                                     nn.GELU(),
+                                     nn.Dropout(0.5),
+                                     nn.BatchNorm2d(64),
+                                     nn.ConvTranspose2d(64,32,kernel_size=3, stride=2, padding=0),
+                                     nn.GELU(),
+                                     nn.Dropout(0.5),
+                                     nn.BatchNorm2d(32),
+                                     nn.ConvTranspose2d(32,16,kernel_size=3, stride=2, padding=0),
+                                     nn.GELU(),
+                                     nn.Dropout(0.5),
+                                     nn.BatchNorm2d(16),                                    
+                                     nn.ConvTranspose2d(16,1,kernel_size=4, stride=1, padding=1),
+                                     nn.Sigmoid()
+                                     )
+        self.device = device
+
+    def forward(self, emb, ori):
+        bs = len(emb)
+        emb = emb.view(bs,64,1,1)
+        img = self.decoder(emb)
+        
+        criterion_DANN = nn.MSELoss().to(self.device)
+        
+        loss = criterion_DANN(img,ori)
+        
+        return loss, img
     
 class GnLD(nn.Module):
     def __init__(self):
         super(GnLD, self).__init__()
-        self.GLD = nn.Sequential(nn.Linear(1088, 64),
+        # self.GLD = nn.Sequential(nn.Linear(1088, 64),
+        #                          nn.GELU(),
+        #                          nn.Dropout(0.5),
+        #                          nn.LayerNorm(64),
+        #                          nn.Linear(64, 64),
+        #                          nn.GELU(),
+        #                          nn.Dropout(0.5),
+        #                          nn.LayerNorm(64),
+        #                          nn.Linear(64, 32),
+        #                          nn.GELU(),
+        #                          nn.Dropout(0.5),
+        #                          nn.LayerNorm(32),                                 
+        #                          nn.Linear(32, 2),
+        #                          )
+        self.GLD = nn.Sequential(nn.Conv2d(65, 128, 3),
                                  nn.GELU(),
                                  nn.Dropout(0.5),
-                                 nn.LayerNorm(64),
-                                 nn.Linear(64, 64),
+                                 nn.BatchNorm2d(128),
+                                 nn.Conv2d(128, 64, 3),
                                  nn.GELU(),
                                  nn.Dropout(0.5),
-                                 nn.LayerNorm(64),
-                                 nn.Linear(64, 32),
+                                 nn.BatchNorm2d(64),
+                                 nn.MaxPool2d(2),
+                                 nn.Conv2d(64, 32, 3),
                                  nn.GELU(),
                                  nn.Dropout(0.5),
-                                 nn.LayerNorm(32),                                 
-                                 nn.Linear(32, 2),
+                                 nn.BatchNorm2d(32),
+                                 nn.MaxPool2d(2),
+                                 nn.Conv2d(32, 16, 3),
+                                 nn.GELU(),
+                                 nn.Dropout(0.5),
+                                 nn.BatchNorm2d(16),
+                                 nn.Conv2d(16, 2, 4),                                 
                                  )
+
         
-    def forward(self, img, emb):
+    def forward(self, emb, img):
         bs = img.shape[0]
-        img = img.view(bs,-1)
+        
+        emb = emb.unsqueeze(2).unsqueeze(2)
+        emb = emb.expand([*emb.shape[:2],32,32])
         
         EM = torch.cat([img,emb],dim=1)    
         output = self.GLD(EM)
-                     
-        return output
+                            
+        return output.view(bs,-1)
 
 class PriorD(nn.Module):
     def __init__(self):
@@ -202,7 +270,7 @@ def target_real_fake(batch_size, device, soft):
     return soft*t, 1 - soft*t, t, 1-t
     
 class DIM(nn.Module):
-    def __init__(self,device='cpu',alpha=1, beta=1, gamma=10):
+    def __init__(self,device='cuda',alpha=1, beta=1, gamma=1):
         super(DIM, self).__init__()
         self.device = device
         self.alpha = alpha
@@ -277,33 +345,18 @@ class DIM(nn.Module):
                                                                               GLD1_loss.item(),
                                                                               prior_loss.item()
                                                                               ))
-                print(EDisease[0,0,:24])
-                print(EDisease[1,0,:24])
-                print('GLD0',GLD0[:2])#,true_em,true_domain)
-                print('GLD1',GLD1[:2])#,fake_em,fake_domain)
+                print(EDisease[0])
+                print(EDisease[1])
+                print('GLD0',GLD0)#,true_em,true_domain)
+                print('GLD1',GLD1)#,fake_em,fake_domain)
                 #print('pred_domain_T',pred_domain_T)
         return GLD_loss+prior_loss
 
-
-E = encoder()
-D = DIM()
-
-try: 
-    E = load_checkpoint(checkpoint_file,'EDisease.pth',E)
-    print(' ** Complete Load EDisease Model ** ')
-except:
-    print('*** No Pretrain_EDisease_Model ***')
-
-try:     
-    D = load_checkpoint(checkpoint_file,'DIM.pth',D)
-except:
-    print('*** No Pretrain_DIM_Model ***')
-    pass
-
-
 def test_AIemb(DS_model,
                dloader,
-               ep):
+               ep,
+               picpath):
+    global device
     DS_model.eval()
     
     f_target_ = []
@@ -312,6 +365,7 @@ def test_AIemb(DS_model,
     with torch.no_grad():         
         for batch_idx, data in enumerate(dloader):            
             sample,label = data
+            sample,label = sample.to(device),label.to(device)
             imgE, _ = E(sample)   
             
             f_target_.append(imgE.cpu())
@@ -320,14 +374,15 @@ def test_AIemb(DS_model,
     f_target = torch.cat(f_target_,dim=0)    
     all_label = torch.cat(all_label_,dim=0)            
 
-    plot_2d(f_target,all_label,ep): 
+    plot_2d(f_target,all_label,ep,picpath)
+    return f_target,all_label
 
 
 def train_AIemb(DS_model,
                 dim_model,
                 dloader,
-                lr=1e-5,
-                epoch=100,
+                lr=lr,
+                epoch=10000,
                 log_interval=10,
                 parallel=parallel):
     global checkpoint_file, data_loader_test
@@ -346,15 +401,17 @@ def train_AIemb(DS_model,
     iteration = 0
     total_loss = []
     
-    debug = []
-    debugnan = []
-    LargeLossDebug =[]
+    # f_target,all_label = test_AIemb(DS_model,
+    #            data_loader_test,
+    #            'init')
+    
     for ep in range(epoch):   
         t0 = time.time()
         epoch_loss = 0
         epoch_cases =0
         DS_model.train()
         dim_model.train()
+        ptloss = False
         
         for batch_idx, data in enumerate(dloader):
             
@@ -366,11 +423,11 @@ def train_AIemb(DS_model,
             loss = 0
 
             mode = 'D' if batch_idx%2==0 else 'G'
-            ptloss = False
+            
             
             imgE, imgM = E(sample)   
             
-            bs = len(s)
+            bs = len(sample)
               
             loss = dim_model(imgE,
                              imgM,
@@ -378,12 +435,12 @@ def train_AIemb(DS_model,
                              ptloss=ptloss
                             )
 
-            loss.backward()
+            loss.sum().backward()
             model_optimizer.step()
             model_optimizer_dim.step()
 
             with torch.no_grad():
-                epoch_loss += loss.item()*bs
+                epoch_loss += loss.sum().item()*bs
                 epoch_cases += bs
 
             if iteration % log_interval == 0:
@@ -391,7 +448,8 @@ def train_AIemb(DS_model,
                         ep, batch_idx * batch_size,
                         100. * batch_idx / len(dloader),
                         (time.time()-t0)*len(dloader)/(60*(batch_idx+1)),
-                        loss.item()))
+                        loss.sum().item()))
+            if iteration % 1000 == 1:
                 ptloss = True
             else:
                 ptloss = False
@@ -428,8 +486,192 @@ def train_AIemb(DS_model,
         pd_total_loss = pd.DataFrame(total_loss)
         pd_total_loss.to_csv('./loss_record/total_loss_finetune.csv', sep = ',')
         
-        test_AIemb(DS_model,
+        f_target,all_label = test_AIemb(DS_model,
                    data_loader_test,
-                   ep)
+                   ep+38,
+                   picpath='./pic/')
     print(total_loss)
+
+
+def train_AIAED(DS_model,
+                dim_model,
+                dloader,
+                lr=lr,
+                epoch=10000,
+                log_interval=10,
+                parallel=parallel):
+    global checkpoint_file, data_loader_test
+    DS_model.to(device)
+    dim_model.to(device)
+
+    model_optimizer = optim.Adam(DS_model.parameters(), lr=lr)
+    model_optimizer_dim = optim.Adam(dim_model.parameters(), lr=lr)
+    if parallel:
+        DS_model = torch.nn.DataParallel(DS_model)
+        dim_model = torch.nn.DataParallel(dim_model)
+    else:
+        if device == 'cuda':
+            torch.cuda.set_device(0)
+
+    iteration = 0
+    total_loss = []
+    
+    f_target,all_label = test_AIemb(DS_model,
+               data_loader_test,
+               'init',
+               picpath='./pic_AE/')
+    
+    for ep in range(epoch):   
+        t0 = time.time()
+        epoch_loss = 0
+        epoch_cases =0
+        DS_model.train()
+        dim_model.train()
+        ptloss = False
+        
+        for batch_idx, data in enumerate(dloader):
+            
+            sample,label = data
+            sample,label = sample.to(device),label.to(device)
+            
+            model_optimizer.zero_grad()
+            model_optimizer_dim.zero_grad()
+            loss = 0
+
+            mode = 'D' if batch_idx%2==0 else 'G'
+            
+            
+            imgE, imgM = E(sample)   
+            
+            bs = len(sample)
+              
+            loss,img = dim_model(imgE,
+                                 sample,
+                                 )
+
+            loss.sum().backward()
+            model_optimizer.step()
+            model_optimizer_dim.step()
+
+            with torch.no_grad():
+                epoch_loss += loss.sum().item()*bs
+                epoch_cases += bs
+
+            if iteration % log_interval == 0:
+                print('Ep:{} [{} ({:.0f}%)/ ep_time:{:.0f}min] L:{:.4f}'.format(
+                        ep, batch_idx * batch_size,
+                        100. * batch_idx / len(dloader),
+                        (time.time()-t0)*len(dloader)/(60*(batch_idx+1)),
+                        loss.sum().item()))
+            if iteration % 1000 == 1:
+                ptloss = True
+            else:
+                ptloss = False
+                
+            if iteration % 500 == 0:
+                try:  
+                    save_checkpoint(checkpoint_file=checkpoint_file,
+                                    checkpoint_path='EDisease.pth',
+                                    model=DS_model,
+                                    parallel=parallel)
+                    save_checkpoint(checkpoint_file=checkpoint_file,
+                                    checkpoint_path='decoder.pth',
+                                    model=dim_model,
+                                    parallel=parallel)
+                except: 
+                    print('** error save checkpoint **')
+                    pass
+                
+            iteration +=1
+        if ep % 1 ==0:
+            save_checkpoint(checkpoint_file=checkpoint_file,
+                            checkpoint_path='EDisease.pth',
+                            model=DS_model,
+                            parallel=parallel)
+            save_checkpoint(checkpoint_file=checkpoint_file,
+                            checkpoint_path='decoder.pth',
+                            model=dim_model,
+                            parallel=parallel)
+
+            print('======= epoch:%i ========'%ep)
+                 
+        print('++ Ep Time: {:.1f} Secs ++'.format(time.time()-t0)) 
+        total_loss.append(float(epoch_loss/epoch_cases))
+        pd_total_loss = pd.DataFrame(total_loss)
+        pd_total_loss.to_csv('./loss_record/total_loss_finetune.csv', sep = ',')
+        
+        f_target,all_label = test_AIemb(DS_model,
+                   data_loader_test,
+                   ep,picpath='./pic_AE/')
+    print(total_loss)
+    
+if task == 'dim':    
+    device = 'cuda:1'
+    checkpoint_file = './checkpoint' 
+
+    E = encoder()
+    D = DIM(device=device,gamma=1)
+    
+    dec = decoder()
+    
+    try: 
+        E = load_checkpoint(checkpoint_file,'EDisease.pth',E)
+        print(' ** Complete Load EDisease Model ** ')
+    except:
+        print('*** No Pretrain_EDisease_Model ***')
+    
+    try:     
+        D = load_checkpoint(checkpoint_file,'DIM.pth',D)
+    except:
+        print('*** No Pretrain_DIM_Model ***')
+        pass
+    
+    try:     
+        dec = load_checkpoint(checkpoint_file,'decoder.pth',dec)
+    except:
+        print('*** No Pretrain_decoder_Model ***')
+        pass
+
+    train_AIemb(E,
+                D, 
+                data_loader_train,
+                lr=1e-5, 
+                epoch=100,
+                log_interval=10,
+                parallel=parallel)
+    
+elif task == 'ae': 
+    device = 'cuda'
+    checkpoint_file = './checkpoint_AE' 
+    
+    E = encoder()
+    D = DIM(device=device,gamma=1)
+    
+    dec = decoder()
+    
+    try: 
+        E = load_checkpoint(checkpoint_file,'EDisease.pth',E)
+        print(' ** Complete Load EDisease Model ** ')
+    except:
+        print('*** No Pretrain_EDisease_Model ***')
+    
+    try:     
+        D = load_checkpoint(checkpoint_file,'DIM.pth',D)
+    except:
+        print('*** No Pretrain_DIM_Model ***')
+        pass
+    
+    try:     
+        dec = load_checkpoint(checkpoint_file,'decoder.pth',dec)
+    except:
+        print('*** No Pretrain_decoder_Model ***')
+        pass
+
+    train_AIAED(E,
+                dec, 
+                data_loader_train,
+                lr=1e-5, 
+                epoch=100,
+                log_interval=10,
+                parallel=parallel)
 
